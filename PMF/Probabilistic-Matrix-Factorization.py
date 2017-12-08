62c533452a9194a1fea09fb03203f60101773a43
zhangwenzhong <33918658+zhangwenzhong@users.noreply.github.com>
2017-12-08 12:55:04 +0800

    final
    change

--- PMF/Probabilistic-Matrix-Factorization.py
+++ PMF/Probabilistic-Matrix-Factorization.py
@@ -6,10 +6,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from numpy import *
-from LoadData import load_rating_data, spilt_rating_dat
 from dataStructure import GeneralTool
 import time
 
+import random
+def load_rating_data(file_path='data.txt'):
+
+    prefer = []
+    for line in open(file_path, 'r'):  
+        (userid, itemid, rating, date) = line.split('\t')  
+        uid = int(userid)
+        iid = int(itemid)
+        rat = float(rating)
+        prefer.append([uid, iid, rat])
+    data = array(prefer)
+    return data
+def spilt_rating_dat(data, size=0.2):
+    train_data = []
+    test_data = []
+    for line in data:
+        rand = random.random()
+        if rand < size:
+            test_data.append(line)
+        else:
+            train_data.append(line)
+    train_data = array(train_data)
+    test_data = array(test_data)
+    return train_data, test_data
 
 class PMF(object):
     def __init__(self, num_feat=50, epsilon=0.8, _lambda=0.5, momentum=0.8,
@@ -17,7 +40,7 @@ class PMF(object):
                  batch_size=1000):
         self.num_feat = num_feat  # Number of latent features,
         self.epsilon = epsilon  # learning rate,
-        self._lambda = _lambda  # L2 regularization,
+        self._lambda = _lambda  # regularization,
         self.momentum = momentum  # momentum of the gradient,
         self.maxepoch = maxepoch  # Number of epoch before stop,
         self.num_batches = num_batches  # Number of batches in each epoch (for SGD optimization),
@@ -34,46 +57,45 @@ class PMF(object):
         self.train_rmse = []
         self.test_rmse = []
 
-    # ***Fit the model with train_tuple and evaluate RMSE on both train and validation data.  ***********#
-    # ***************** train_vec=TrainData, val_vec=TestData*************#
+
     def fit(self, train_vec, val_vec):
         # mean subtraction
-        self.mean_inv = np.mean(train_vec[:, 2])  # 评分平均值
+        self.mean_inv = np.mean(train_vec[:, 2])  # mean rating
 
-        pairs_tr = train_vec.shape[0]  # traindata 中条目数
-        pairs_va = val_vec.shape[0]  # testdata中条目数
+        pairs_tr = train_vec.shape[0]  # traindata length
+        pairs_va = val_vec.shape[0]  # testdata length
 
         # 1-p-i, 2-m-c
 
-        num_user = int(max(np.amax(train_vec[:, 0]), np.amax(val_vec[:, 0]))) + 1  # 第0列，user总数
-        num_item = int(max(np.amax(train_vec[:, 1]), np.amax(val_vec[:, 1]))) + 1  # 第1列，movie总数
+        num_user = int(max(np.amax(train_vec[:, 0]), np.amax(val_vec[:, 0]))) + 1  
+        num_item = int(max(np.amax(train_vec[:, 1]), np.amax(val_vec[:, 1]))) + 1  
 
         incremental = False
         if ((not incremental) or (self.w_C is None)):
             # initialize
             self.epoch = 0
-            self.w_C = 0.1 * np.random.randn(num_item, self.num_feat)  # numpy.random.randn 电影 M x D 正态分布矩阵
-            self.w_I = 0.1 * np.random.randn(num_user, self.num_feat)  # numpy.random.randn 用户 N x D 正态分布矩阵
+            self.w_C = 0.1 * np.random.randn(num_item, self.num_feat)  
+            self.w_I = 0.1 * np.random.randn(num_user, self.num_feat)  
 
-            self.w_C_inc = np.zeros((num_item, self.num_feat))  # 创建电影 M x D 0矩阵
-            self.w_I_inc = np.zeros((num_user, self.num_feat))  # 创建用户 N x D 0矩阵
+            self.w_C_inc = np.zeros((num_item, self.num_feat))  #  M x D 0 matrix
+            self.w_I_inc = np.zeros((num_user, self.num_feat))  # N x D 0 matrix
 
         while self.epoch < self.maxepoch:
             self.epoch += 1
 
             # Shuffle training truples
-            shuffled_order = np.arange(train_vec.shape[0])  # 创建等差array
-            np.random.shuffle(shuffled_order)  # 用于将一个列表中的元素打乱
+            shuffled_order = np.arange(train_vec.shape[0])  
+            np.random.shuffle(shuffled_order)  # permutation
 
             # Batch update
             for batch in range(self.num_batches):
                 # print "epoch %d batch %d" % (self.epoch, batch+1)
                 batch_idx = np.mod(np.arange(self.batch_size * batch, self.batch_size * (batch + 1)),
-                                   shuffled_order.shape[0])  # 本次迭代要使用的索引下标
+                                   shuffled_order.shape[0])  # index for this epoch
                 batch_invID = np.array(train_vec[shuffled_order[batch_idx], 0], dtype='int32')
                 batch_comID = np.array(train_vec[shuffled_order[batch_idx], 1], dtype='int32')
 
-                # Compute Objective Function
+                # Compute Objective Function(error）
                 pred_out = np.sum(np.multiply(self.w_I[batch_invID, :], self.w_C[batch_comID, :]),
                                   axis=1)  # mean_inv subtracted
                 rawErr = pred_out - train_vec[shuffled_order[batch_idx], 2] + self.mean_inv
@@ -119,65 +141,33 @@ class PMF(object):
                 if batch == self.num_batches - 1:
                     print(self.epoch)
                     localtime = time.asctime(time.localtime(time.time()))
-                    print("本地时间为 :" + str(localtime))
+                    print("Local time is :" + str(localtime))
                     print('Training RMSE: %f, Test RMSE %f' % (self.err_train[-1], self.err_val[-1]))
                     self.train_rmse.append(self.err_train[-1])
                     self.test_rmse.append(self.err_val[-1])
                     # ****************Predict rating of all movies for the given user. ***************#
 
     def predict(self, invID):
-        return np.dot(self.w_C, self.w_I[int(invID), :]) + self.mean_inv  # numpy.dot 点乘
+        return np.dot(self.w_C, self.w_I[int(invID), :]) + self.mean_inv  
 
-    # ****************Set parameters by providing a parameter dictionary.  ***********#
-#     def set_params(self, parameters):
-#         if isinstance(parameters, dict):
-#             self.num_feat = parameters.get("num_feat", 10)
-#             self.epsilon = parameters.get("epsilon", 1)
-#             self._lambda = parameters.get("_lambda", 0.1)
-#             self.momentum = parameters.get("momentum", 0.8)
-#             self.maxepoch = parameters.get("maxepoch", 20)
-#             self.num_batches = parameters.get("num_batches", 10)
-#             self.batch_size = parameters.get("batch_size", 1000)
 
-    def topK(self, model, test_vec, k=10):  # model TrainDataSet, test_vec
-        inv_lst = np.unique(test_vec[:, 0])
-        pred = {}
-        for inv in inv_lst:
-            if pred.get(inv, None) is None:
-                pred[inv] = np.argsort(self.predict(inv))[-k:]  # numpy.argsort索引排序
-
-        intersection_cnt = {}
-        for i in range(test_vec.shape[0]):
-            if test_vec[i, 1] in pred[test_vec[i, 0]]:
-                intersection_cnt[test_vec[i, 0]] = intersection_cnt.get(test_vec[i, 0], 0) + 1
-        invPairs_cnt = np.bincount(np.array(test_vec[:, 0], dtype='int32'))
-
-        precision_acc = 0.0
-        recall_acc = 0.0
-        for inv in inv_lst:
-            precision_acc += intersection_cnt.get(inv, 0) / float(k)
-            recall_acc += intersection_cnt.get(inv, 0) / float(invPairs_cnt[int(inv)])
-
-        return precision_acc / len(inv_lst), recall_acc / len(inv_lst)
 
 
 if __name__ == "__main__":
-    test_file_path = "testResult" + os.sep
-#    file_path = GeneralTool.getDataPath("review_final.txt")
-    file_path = GeneralTool.getDataPath("review_final_part(30000user30000business).txt")
+
+    file_path = GeneralTool.getDataPath("review_final.txt")
+##    file_path = GeneralTool.getDataPath("review_final_part(30000user30000business).txt")
     pmf = PMF(num_feat=75, epsilon=0.08, _lambda=0.8, momentum=0.8,
-                 maxepoch=100, num_batches=10,
+                 maxepoch=1000, num_batches=10,
                  batch_size=1000)
     ratings = load_rating_data(file_path)
     print(len(np.unique(ratings[:, 0])), len(np.unique(ratings[:, 1])), pmf.num_feat)
     train, test = spilt_rating_dat(ratings, 0.05)
     pmf.fit(train, test)
-    print("Lenlen(pmf.w_C)"+str(len(pmf.w_C)))
-    print("Lenlen(pmf.w_I)"+str(len(pmf.w_I)))
-    # for i in range(0,10):
-     #   print(pmf.w_C[i])
-#     out1=open("item_vector30000.txt",'w')F
-    out1_filename = test_file_path + "item_vector_all.txt"
+    print("Len(pmf.w_C)"+str(len(pmf.w_C)))
+    print("Len(pmf.w_I)"+str(len(pmf.w_I)))
+#Output result
+    out1_filename ="item_vector_all.txt"
     out1 = open(GeneralTool.getDataPath(out1_filename), 'w')
     buffer = 0
     for i in pmf.w_C:
@@ -189,9 +179,9 @@ if __name__ == "__main__":
         if buffer == 5000:
             out1.flush()
             buffer = 0
-            print('flush5000')
+            #print('flush5000')
     out1.close()
-    out2_filename = test_file_path + "user_vector_all.txt"
+    out2_filename ="user_vector_all.txt"
     out2 = open(GeneralTool.getDataPath(out2_filename), 'w')
     buffer = 0
     for i in pmf.w_I:
@@ -203,9 +193,9 @@ if __name__ == "__main__":
         if buffer == 5000:
             out2.flush()
             buffer = 0
-            print('flush5000')
+            #print('flush5000')
     out2.close()
-    out3_filename = test_file_path + "global_bias.txt"
+    out3_filename ="global_bias.txt"
     out3 = open(GeneralTool.getDataPath(out3_filename), 'w')
     out3.write(str(pmf.mean_inv))
     out3.close()
